862b4383f8367505f28b482bba41985b09617cc2
Oazish <oazish@users.noreply.github.com>
2019-07-05 19:08:47 -0700

    Responsive quotes list and category detail page
    - Prevent long quote text from stretching the height while inside of quote list.
    - Make navbar collapsible at small screen sizes.
    - Move "All Quotes" page from /quotes/ to /all-quotes/ to prevent conflict with site path prefix per https://github.com/gatsbyjs/gatsby/issues/15334.
    - Add Bootstrap peer dependencies, JQuery and Popper.js.
    - Include Bootstrap JS, necessary for expanding/contracting navbar.

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,20 +3,33 @@ import { Link } from 'gatsby';
 
 export default ({ children }) => (
   <div>
-    <nav className="navbar navbar-expand navbar-light bg-light">
-      <Link to="/" className="navbar-brand nav-item nav-link" activeClassName="active">
+    <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <Link to="/" className="navbar-brand" activeClassName="active">
         Spiritual Quotes
       </Link>
-      <div className="navbar-nav">
-        <Link to="/quotes/" className="nav-item nav-link" activeClassName="active">
-          All Quotes
-        </Link>
-        <Link to="/categories/" className="nav-item nav-link" activeClassName="active">
-          Quote Categories
-        </Link>
-        <Link to="/authors/rama/" className="nav-item nav-link" activeClassName="active">
-          About Rama
-        </Link>
+      <button
+        type="button"
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-nav">
+          <Link to="/all-quotes/" className="nav-item nav-link" activeClassName="active">
+            All Quotes
+          </Link>
+          <Link to="/categories/" className="nav-item nav-link" activeClassName="active">
+            Quote Categories
+          </Link>
+          <Link to="/authors/rama/" className="nav-item nav-link" activeClassName="active">
+            About Rama
+          </Link>
+        </div>
       </div>
     </nav>
     <div className="p-3">
--- src/components/quotes-list.js
+++ src/components/quotes-list.js
@@ -5,49 +5,53 @@ import { FaShareAltSquare } from 'react-icons/fa';
 export default ({ markdownRemarkNodes: quotes }) => (
   <div className="d-flex flex-column">
       {quotes.map(quote =>
-        <div className="container-fluid p-0">
-        <div className="row mb-2 border shadow-sm">
-          <Link
-            key={quote.fields.slug}
-            to={quote.fields.slug}
-            className="col d-flex p-0 align-items-center text-decoration-none"
-          >
-            <QuoteImage quote={quote} />
-            <div className="m-3">
-              {quote.excerpt}
+        <div key={quote.fields.slug} className="container-fluid p-0">
+          <div className="row mb-2 border shadow-sm">
+            <Link
+              key={quote.fields.slug}
+              to={quote.fields.slug}
+              className="col d-flex p-0 text-decoration-none"
+            >
+              <QuoteImage quote={quote} />
+              {/* Necessary to keep long quotes from expanding container. */}
+              <div
+                className="mx-3 position-relative h-100 flex-fill"
+              >
+                <div
+                  className="position-absolute overflow-hidden align-items-center d-flex"
+                  style={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                >
+                  {quote.excerpt}
+                </div>
+              </div>
+            </Link>
+            <div className="col-auto d-flex align-items-center">
+              <FaShareAltSquare size="32px" color="darkgray" />
             </div>
-          </Link>
-          <div className="col-auto d-flex align-items-center">
-            <FaShareAltSquare size="32px" color="darkgray" />
           </div>
-        </div>
         </div>
       )}
   </div>
 );
 
-const QuoteImage = ({ quote }) => {
-  const visual = quote.image
-    ? (
-      <div
-        style={{
-          backgroundImage: `url('${quote.image.publicURL}')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          width: '212px',
-          height: '190px',
-        }}
-      />
-    )
-    : (
-      <div
-        style={{
-          width: '212px',
-          height: '190px',
-          backgroundColor: quote.color,
-        }}
-        className="align-middle d-inline-block"
-      />
-    );
-  return <div className="d-inline-block">{visual}</div>;
-};
+const QuoteImage = ({ quote }) => (
+  <div className="d-inline-block">
+    <div
+      style={{
+        width: '212px',
+        height: '190px',
+        ...(
+          quote.image
+          ? {
+            backgroundImage: `url('${quote.image.publicURL}')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }
+          : {
+            backgroundColor: quote.color,
+          }
+        )
+      }}
+    />
+  </div>
+);
