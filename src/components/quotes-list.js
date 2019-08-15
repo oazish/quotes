493933feffa8f8c93643395e52f95d3f3e5d291f
Oazish <oazish@users.noreply.github.com>
2019-08-15 02:30:30 -0700

    Misc refinements to quote list
    - Simplify from Bootstrap grid to vanilla flex
    - Remove border since drop shadow is in place
    - Fix color on share button when other parts of quote link are hovered

--- src/components/quotes-list.js
+++ src/components/quotes-list.js
@@ -1,37 +1,30 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import classNames from 'classnames';
 
 import QuoteImage from './quote-image';
 import ShareButton from './share-button';
 import styles from '../styles/quotes-list.module.css';
 
 export default ({ quotes }) => (
-  <div className="d-flex mt-2 flex-column">
+  <div className={styles.quotes}>
     {quotes.map(quote =>
-      <div key={quote.fields.slug} className="container-fluid">
-        <div className={classNames('row mb-2 border', styles.quote)}>
-          <Link
-            key={quote.fields.slug}
-            to={quote.fields.slug}
-            className="col d-flex p-0 text-decoration-none"
-          >
-            <div className={styles.quoteImage}>
-              <QuoteImage quote={quote} />
-            </div>
-            {/* Necessary to keep long quotes from expanding container. */}
-            <div className="mx-3 position-relative h-100 flex-fill">
-              <div className={styles.quoteText}>
-                {quote.excerpt}
-              </div>
-            </div>
-            <ShareButton
-              shareUrl={quote.fields.slug}
-              className="col-auto d-flex align-items-center"
-            />
-          </Link>
+      <Link
+        key={quote.fields.slug}
+        to={quote.fields.slug}
+      >
+        <div className={styles.image}>
+          <QuoteImage quote={quote} />
         </div>
-      </div>
+        <div className={styles.text}>
+          <q>
+            {quote.excerpt}
+          </q>
+        </div>
+        <ShareButton
+          shareUrl={quote.fields.slug}
+          className={styles.share}
+        />
+      </Link>
     )}
   </div>
 );
