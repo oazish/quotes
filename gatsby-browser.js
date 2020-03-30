5c32a4c5c7a7e60fef167fd182b6dab885aae578
Oazish <oazish@users.noreply.github.com>
2020-03-29 19:24:22 -0700

    Track page views in Google Analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 import './src/styles/global.css';
 
-// TODO: PUT REDIRECT FOR QUOTES PAGE IF POSSIBLE!
+function PageWrapper({ children, location: { pathname: url } }) {
+  useEffect(
+    () => ReactGA.initialize('UA-158589454-4'),
+    [],
+  );
+  useEffect(
+    () => ReactGA.pageview(url),
+    [url],
+  );
+  return children;
+}
+
+export function wrapPageElement({ element, props }) {
+  return <PageWrapper {...props}>{element}</PageWrapper>;
+}
